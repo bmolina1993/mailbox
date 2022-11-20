e627b07d1da2dad4f358fc6e4774f8c9379e38d2
Braian Molina <braian_gabriel_1993@hotmail.com>
2022-11-20 15:43:06 -0300

    se crea proyecto de vue+tailwindcss

---
+++ front_end/tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      backgroundImage: {
+        // sanFrancisco: "url('/public/img/sanFrancisco.jpg')",
+      },
+      backgroundColor: (theme) => ({
+        ...theme("colors"),
+        primary: "#CC2D4A",
+        secondary: "#8FA206",
+        tertiary: "#61AEC9",
+      }),
+      textColor: {
+        primary: "#CC2D4A",
+        secondary: "#8FA206",
+        tertiary: "#61AEC9",
+      },
+      fontFamily: {
+        Montserrat: ["Montserrat", "sans-serif"],
+      },
+    },
+  },
+  /*
+  plugins: [require("tailwind-scrollbar")],
+  variants: {
+    scrollbar: ["rounded"],
+  },
+  */
+};
