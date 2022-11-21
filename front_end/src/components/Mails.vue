6645c26e13d1c326c1cdb37d2b7d1448ca6f3ab3
Braian Molina <braian_gabriel_1993@hotmail.com>
2022-11-21 16:02:03 -0300

    se agrega provide/inject para fetch api

--- front_end/src/components/Mails.vue
+++ front_end/src/components/Mails.vue
@@ -1,4 +1,9 @@
-<script setup></script>
+<script setup>
+import { inject, onBeforeMount } from "vue";
+
+const data = inject("dataAPI");
+</script>
+
 <template>
   <div class="h-20 w-full bg-darkPrimary px-5 py-2">
     <div style="border: 1px solid" class="h-full w-full"></div>
