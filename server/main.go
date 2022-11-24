0235e90b2c09f924afd354c20efd6c8e181bcec1
Braian Molina <braian_gabriel_1993@hotmail.com>
2022-11-24 19:37:03 -0300

    se habilita CORS

--- server/main.go
+++ server/main.go
@@ -7,6 +7,7 @@ import (
 	service "github.com/bmolina1993/mailbox/src/services"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
 )
 
 func main() {
@@ -19,6 +20,16 @@ func main() {
 	// habilita servidor con data recolectada
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
+
+	//habilita cors
+	r.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+	}))
+
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte(fmt.Sprintf("%v", data)))
 	})
